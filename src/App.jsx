3fc2571ef7e5c8003ddca8272edb5c6c8969a28f
Noran Sherif <>
2023-12-06 17:39:37 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import Product from "./pages/Product";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+import { CitiesProvider } from "./context/citiesContext";
+
+function App() {
+  return (
+    <div>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="*" element={<PageNotFound />} />
+            <Route path="product" element={<Product />} />
+            <Route path="app" element={<AppLayout />}>
+              <Route index element={<Navigate replace to="cities" />} />
+              <Route path="cities" element={<CityList />} />
+
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </div>
+  );
+}
+
+export default App;
