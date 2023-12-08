35da585d9f510a9c2efccf212e64efa04f25df0d
Noran Sherif <>
2023-12-08 23:16:36 +0200

    add fake auth: implementing login

--- src/App.jsx
+++ src/App.jsx
@@ -11,29 +11,32 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./context/citiesContext";
+import { AuthProvider } from "./context/FakeAuthContext";
 
 function App() {
   return (
     <div>
-      <CitiesProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="*" element={<PageNotFound />} />
-            <Route path="product" element={<Product />} />
-            <Route path="app" element={<AppLayout />}>
-              <Route index element={<Navigate replace to="cities" />} />
-              <Route path="cities" element={<CityList />} />
+      <AuthProvider>
+        <CitiesProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="*" element={<PageNotFound />} />
+              <Route path="product" element={<Product />} />
+              <Route path="app" element={<AppLayout />}>
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
 
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="login" element={<Login />} />
-          </Routes>
-        </BrowserRouter>
-      </CitiesProvider>
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="login" element={<Login />} />
+            </Routes>
+          </BrowserRouter>
+        </CitiesProvider>
+      </AuthProvider>
     </div>
   );
 }
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,14 @@
 import AppNav from "../components/AppNav";
 import Map from "../components/Map";
 import SideBar from "../components/SideBar";
+import User from "../components/User";
 import styles from "./AppLayout.module.css";
 function AppLayout() {
   return (
     <div className={styles.app}>
       <SideBar />
       <Map />
+      <User />
     </div>
   );
 }
